f8334a96d54fd9da1275e1da5b919d072ecf2bc4
Cassio <cassiojr0108@gmail.com>
2025-05-27 11:53:04 -0300

    feat(): email service exception error

---
+++ src/email-service/domain/errors/emailServiceException.error.ts
@@ -0,0 +1,14 @@
+export class EmailServiceException extends Error {
+  public status: number
+  public error?: any
+  constructor(
+    public message: string,
+    error?: any,
+    status?: number,
+  ) {
+    super(message)
+    this.name = 'EmailServiceException'
+    this.error = error
+    this.status = status ?? 500
+  }
+}
