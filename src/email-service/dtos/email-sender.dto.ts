b00bedb1752b7aea941de3d43c3bc937b3c6a5dd
Cassio <cassiojr0108@gmail.com>
2025-05-28 16:26:41 -0300

    feat(): Validate data of dto

--- src/email-service/dtos/email-sender.dto.ts
+++ src/email-service/dtos/email-sender.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator'
+
 export class EmailSenderDto {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   to: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   subject: string
+
+  @IsString()
+  @IsNotEmpty()
   body: string
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EmailServiceFilter } from './email-service/infrastructure/exception-filters/email-service-error/email-service-error.filter'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
   app.useGlobalFilters(new EmailServiceFilter())
   await app.listen(process.env.PORT ?? 3000)
 }
